9bb86a4c06f4daf807e1336f3577a67c29f3868e
Brenner Vaz <brennerduartevaz@hotmail.com>
2023-02-23 23:01:41 -0300

    fix: exports and barrels

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/*.test.ts'],
-  collectCoverageFrom: ['src/**/*.ts'],
-  coverageReporters: ['text', 'lcov'],
-}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { P2PDataChannel } from '@src/services'
 
+export * from '@src/config'
+export * from '@src/exceptions'
 export * from '@src/types'
 export * from '@src/services'
 export default P2PDataChannel
