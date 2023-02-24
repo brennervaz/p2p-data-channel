99b0d4ea3c99ebfdaa654b83d11ea91b876731c8
Brenner Vaz <brennerduartevaz@hotmail.com>
2023-02-23 23:45:17 -0300

    fix: store connections in map instead of array

--- src/services/ConnectionService.ts
+++ src/services/ConnectionService.ts
@@ -4,27 +4,27 @@ import { IConnection, IConnectionService, PeerId } from '@src/types'
 
 export class ConnectionService<IConnectionType> implements IConnectionService<IConnectionType> {
   private logService = new LogService(ConnectionService.name)
-  private peerConnections: IConnection<IConnectionType>[] = []
+  private peerConnections = new Map<PeerId, IConnection<IConnectionType>>()
 
   addConnection(peerId: PeerId, connection: IConnectionType): void {
     const peerConnection: IConnection<IConnectionType> = { peerId, connection }
-    this.peerConnections.push(peerConnection)
+    this.peerConnections.set(peerId, peerConnection)
     this.logService.log('added connection', peerConnection)
   }
 
   removeConnection(peerId: PeerId): void {
-    this.peerConnections = this.peerConnections.filter(c => c.peerId !== peerId)
+    this.peerConnections.delete(peerId)
     this.logService.log('removed connection', peerId)
   }
 
   getConnection(peerId: PeerId): IConnectionType {
-    const peerConnection = this.peerConnections.find(c => c.peerId === peerId)
+    const peerConnection = this.peerConnections.get(peerId)
     if (!peerConnection) throw new ConnectionNotEstablished(`Connection not found for peer ${peerId}`)
     this.logService.debug('connection', peerConnection)
     return peerConnection.connection
   }
 
   getAll(): IConnectionType[] {
-    return this.peerConnections.map(p => p.connection)
+    return Array.from(this.peerConnections.values()).map(p => p.connection)
   }
 }
